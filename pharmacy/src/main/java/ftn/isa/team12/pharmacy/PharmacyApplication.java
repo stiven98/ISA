437d2231fae512f8d11f6f80982838883f1ece8a
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2021-01-10 13:52:40 +0100

    :tada: Projects initialization

---
+++ pharmacy/src/main/java/ftn/isa/team12/pharmacy/PharmacyApplication.java
@@ -0,0 +1,13 @@
+package ftn.isa.team12.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyApplication.class, args);
+	}
+
+}
