4dae626ba82ba9833ee2c9be7032779e2862e28e
qndaa <qndaa@uns.ac.rs>
2021-01-27 17:37:12 +0100

    :construction: :beer: Login page and router in angular

--- pharmacy-frontend/src/app/app.module.ts
+++ pharmacy-frontend/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: SingInComponent },
+  { path: 'registration', component: SingUpComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SingInComponent,
+    SingUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
