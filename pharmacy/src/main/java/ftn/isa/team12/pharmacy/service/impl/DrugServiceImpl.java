354d7dea57e028928356b7c390b083d78b1842b9
Aca Simic <acasimic98@uns.ac.rs>
2021-01-28 14:36:04 +0100

    :recycle: :construction_worker: Add DrugService

---
+++ pharmacy/src/main/java/ftn/isa/team12/pharmacy/service/impl/DrugServiceImpl.java
@@ -0,0 +1,23 @@
+package ftn.isa.team12.pharmacy.service.impl;
+
+import ftn.isa.team12.pharmacy.domain.drugs.Drug;
+import ftn.isa.team12.pharmacy.domain.drugs.DrugReservation;
+import ftn.isa.team12.pharmacy.repository.DrugRepository;
+import ftn.isa.team12.pharmacy.service.DrugService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DrugServiceImpl implements DrugService {
+
+    @Autowired
+    private DrugRepository drugRepository;
+    
+    
+    @Override
+    public List<Drug> findAll() {
+        return drugRepository.findAll();
+    }
+}
