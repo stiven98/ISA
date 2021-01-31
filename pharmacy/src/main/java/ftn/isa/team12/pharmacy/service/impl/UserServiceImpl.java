82d8d51bd3c1bb9007d1d1daba3928176d26af65
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2021-02-01 00:23:24 +0100

    :bug: Minor fix

--- pharmacy/src/main/java/ftn/isa/team12/pharmacy/service/impl/UserServiceImpl.java
+++ pharmacy/src/main/java/ftn/isa/team12/pharmacy/service/impl/UserServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.UUID;
