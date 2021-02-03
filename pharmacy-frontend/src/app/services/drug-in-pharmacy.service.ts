ac2d6786f526c528922dd428c8e12f2a4d50b842
Aca Simic <acasimic98@uns.ac.rs>
2021-02-03 22:30:43 +0100

    :sparkles: Implemented search drugs in pharmacy

--- pharmacy-frontend/src/app/services/drug-in-pharmacy.service.ts
+++ pharmacy-frontend/src/app/services/drug-in-pharmacy.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DrugOrderModel } from '../drug-order/drugOrderModel';
 import { DrugInpharmacyChangeModel } from '../ph-admin/drug-in-pharmacy/drugInPharmacyModel';
+import { SearchDrugModel } from '../ph-admin/drug-in-pharmacy/searchDrugModel';
 import { ApiService } from './api.service';
 import { ConfigService } from './config.service';
 
@@ -42,6 +43,15 @@ export class DrugInPharmacyService {
     .pipe(map(drugOrder =>{ 
       return drugOrder;
     }))
+  }
+
+
+  searhcDrugInPharmacy(searchDrug:SearchDrugModel): Observable<any>{
+    return this.apiService.post('http://localhost:8080/api/search/drug',searchDrug)
+    .pipe(map((response:Response) =>{
+      return response;
+    }))
+
   }
 
 
