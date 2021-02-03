e14d181b87201fd1a3595f2b04765de06fbbafd0
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2021-02-03 23:38:41 +0100

    :sparkles: Implemented patients sorting

--- pharmacy-frontend/src/app/services/medical-stuff.service.ts
+++ pharmacy-frontend/src/app/services/medical-stuff.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { PatientClient } from '../shared/models/patientClient';
 import { ApiService } from './api.service';
 import { ConfigService } from './config.service';
 
@@ -10,8 +11,8 @@ import { ConfigService } from './config.service';
 export class MedicalStuffService {
 
   constructor(private api : ApiService, private configService : ConfigService) { }
-
-  getMyPatients() : Observable<any>{
+  
+  getMyPatients() : Observable<PatientClient[]>{
     return this.api.get(this.configService.get_patients_by_medical_stuff).pipe(map(users => {
       return users;
     }));
