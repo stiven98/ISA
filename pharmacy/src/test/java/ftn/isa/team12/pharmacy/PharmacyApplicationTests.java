f293bb234db9e8d2b17344feeebb8d40b0baf807
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2021-02-10 16:52:07 +0100

    :rotating_light: :beer: :lipstick:  Implemented 2 integration and 1 unit test

--- pharmacy/src/test/java/ftn/isa/team12/pharmacy/PharmacyApplicationTests.java
+++ pharmacy/src/test/java/ftn/isa/team12/pharmacy/PharmacyApplicationTests.java
@@ -1,8 +1,10 @@
 package ftn.isa.team12.pharmacy;
-
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PharmacyApplicationTests {
 
