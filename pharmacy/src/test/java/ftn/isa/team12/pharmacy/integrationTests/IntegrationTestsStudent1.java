4bb15bbc035f67b44252f68fa11825bc092682e1
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2021-02-10 20:35:11 +0100

    :rotating_lights: Implemented 5 unit and 5 integration tests for student 1

--- pharmacy/src/test/java/ftn/isa/team12/pharmacy/integrationTests/IntegrationTestsStudent1.java
+++ pharmacy/src/test/java/ftn/isa/team12/pharmacy/integrationTests/IntegrationTestsStudent1.java
@@ -1,19 +1,27 @@
 package ftn.isa.team12.pharmacy.integrationTests;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ftn.isa.team12.pharmacy.domain.common.Location;
+import ftn.isa.team12.pharmacy.domain.enums.UserCategory;
+import ftn.isa.team12.pharmacy.domain.users.*;
 import ftn.isa.team12.pharmacy.dto.LoginDTO;
+import ftn.isa.team12.pharmacy.service.AuthorityService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.transaction.Transactional;
 import java.nio.charset.Charset;
+
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -47,5 +55,56 @@ public class IntegrationTestsStudent1 {
                 .content(objectMapper.writeValueAsString(loginDTO))).andExpect(status().is(200));
 
     }
+    @Test
+    public void testPatientPenalties() throws Exception {
+
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setUsername("maca@faca.com");
+        loginDTO.setPassword("macafaca");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                .contentType(contentType).accept(contentType)
+                .content(objectMapper.writeValueAsString(loginDTO))).andExpect(status().is(200));
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/patient/penalty/"+"maca@faca.com")
+                .contentType(contentType).accept(contentType))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testPatientCategory() throws Exception {
+
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setUsername("maca@faca.com");
+        loginDTO.setPassword("macafaca");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                .contentType(contentType).accept(contentType)
+                .content(objectMapper.writeValueAsString(loginDTO))).andExpect(status().is(200));
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/patient/accountCategory/"+"maca@faca.com")
+                .contentType(contentType).accept(contentType)).andExpect(status().is(200));
+    }
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testGetAllPharmacies() throws Exception {
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/pharmacy/all")
+                .contentType(contentType).accept(contentType)).andExpect(status().is(200));
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testGetDrugByName() throws Exception {
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/drug/name/" + "Aspirin")
+                .contentType(contentType).accept(contentType)).andExpect(status().is(200));
+    }
+
+
 
 }
